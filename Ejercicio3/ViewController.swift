1dddead7e93ed4bc7b5c54483330cfdebdacbfe1
joseromoluque <jose_santiago_romo_m-tsapp1ta2122@cev.com>
2022-10-09 18:16:03 +0200

    controllView FInal

--- Ejercicio3/ViewController.swift
+++ Ejercicio3/ViewController.swift
@@ -9,10 +9,15 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource  {
     
-    @IBOutlet weak var tvOutlet: UITableView!
+    @IBOutlet weak var tvOutlet: UITableView?
     
-    let collection = [Album(name: "Y3Y2", artist: "Rojuu", imageURL: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpbs.twimg.com%2Fmedia%2FFdCxkRxXoAQ5Qkv%3Fformat%3Djpg%26name%3Dmedium&f=1&nofb=1&ipt=9f8e98be263d1449bf53d17627b6816477362c40f4b7576d8465425f6480b576&ipo=images"),
-                      Album(name: "Sour", artist: "Olivia Rodrigo", imageURL: "https://www.gannett-cdn.com/presto/2021/05/21/USAT/86d10a85-a577-426c-aff4-e63b52f2faa3-SOUR_FINAL.jpg?width=1320&height=1320&fit=crop&format=pjpg&auto=webp")]
+    let collection = [
+            Album(name: "CR7", artist: "Cristiano ronaldou", imageURL: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/cristiano-ronaldo-of-portugal-looks-dejected-following-news-photo-1624346993.jpg?crop=1.00xw:1.00xh;0,0&resize=640:*"),
+            Album(name: "MESSI", artist: "Lionel Messi", imageURL: "https://fcb-abj-pre.s3.amazonaws.com/img/jugadors/MESSI.jpg"),
+            Album(name: "CR7", artist: "Cristiano ronaldou", imageURL: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/cristiano-ronaldo-of-portugal-looks-dejected-following-news-photo-1624346993.jpg?crop=1.00xw:1.00xh;0,0&resize=640:*"),
+            Album(name: "MESSI", artist: "Lionel Messi", imageURL: "https://fcb-abj-pre.s3.amazonaws.com/img/jugadors/MESSI.jpg"),
+            Album(name: "CR7", artist: "Cristiano ronaldou", imageURL: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/cristiano-ronaldo-of-portugal-looks-dejected-following-news-photo-1624346993.jpg?crop=1.00xw:1.00xh;0,0&resize=640:*"),
+            Album(name: "MESSI", artist: "Lionel Messi", imageURL: "https://fcb-abj-pre.s3.amazonaws.com/img/jugadors/MESSI.jpg")]
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return collection.count
@@ -36,12 +41,13 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
 
     override func viewDidLoad() {
-        tvOutlet.dataSource = self
-        tvOutlet.delegate = self
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tvOutlet?.dataSource = self
+        tvOutlet?.delegate = self
     }
+ 
 
 
 }
+
 
